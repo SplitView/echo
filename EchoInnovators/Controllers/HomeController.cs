cbb69265b4def53bb221574849883f2cb9e1cea7
Sandesh Karki <sandesh@brainworks.edu.np>
2017-09-12 13:48:48 +0545

    first commit

---
+++ EchoInnovators/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EchoInnovators.Models;
+
+namespace EchoInnovators.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        public string Contact(EmailModel model)
+        {
+            return model.Name;
+        }
+    }
+}
---
+++ EchoInnovators/Models/EmailModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchoInnovators.Models
+{
+    public class EmailModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage= "Please enter the valid Email Address")]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
